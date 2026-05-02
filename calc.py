a77a9dbe74f14b10610d9ae739c820119409f3cb
Андрей Гурьянов <andrei.gurjanov2013@yandex.ru>
2026-05-02 23:06:41 +0300

    fix bugs and add sqrt function

--- calc.py
+++ calc.py
@@ -1,5 +1,5 @@
-# Автор: ...
-
+# Автор: Андрей Гурьянов
+import math
 def add(a, b):
     return a + b
 
@@ -9,9 +9,10 @@ def subtract(a, b):
 
 
 def multiply(a, b):
-    # TODO: не реализовано
-    return None
+    return a * b
 
+def sqrt(x):
+    return math.sqrt(x)
 
 if __name__ == "__main__":
     print("Простой калькулятор запущен.")
